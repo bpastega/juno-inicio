579bdecc8309f367c600ce8680d49e5575bff70a
bpastega <95308940+bpastega@users.noreply.github.com>
2024-10-28 15:01:20 -0300

    consultas list inicial

--- src/app/components/consultas/consultas-list/consultas-list.component.ts
+++ src/app/components/consultas/consultas-list/consultas-list.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PacienteService } from '../../../services/paciente.service';
+import { Consulta } from '../../../models/consulta';
+import { ConsultaService } from '../../../services/consulta.service';
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-consultas-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, NgClass],
   templateUrl: './consultas-list.component.html',
   styleUrl: './consultas-list.component.scss'
 })
 export class ConsultasListComponent {
+
+  lista: Consulta[] = [];
+
+  /*Injections*/
+  pacienteService = inject(PacienteService); //será usado para filtrar consulta com base no nome do paciente
+  consultaService = inject(ConsultaService);
+
+  constructor(){
+    this.findAll();
+  }
+
+  findAll(){
+    this.consultaService.findAll().subscribe({
+      next: lista=>{
+        this.lista=lista;
+      },
+      error: erro =>{
+       // Swal.fire('Erro',erro.error,'error');
+       // alert('Errooooo!!')//futuramente adicionar sweetalert2
+       console.log(erro);
+      }
+    })
+    
+  }
 
 }
