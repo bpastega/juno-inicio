3d595c3a20acef1d6d188499f6093770a9f0d1f3
bpastega <95308940+bpastega@users.noreply.github.com>
2024-10-30 01:27:37 -0300

    inicio form consultas

--- src/app/components/consultas/consultas-form/consultas-form.component.ts
+++ src/app/components/consultas/consultas-form/consultas-form.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
+import { ConsultaService } from '../../../services/consulta.service';
+import { Consulta } from '../../../models/consulta';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { FormsModule } from '@angular/forms';
+import { Paciente } from '../../../models/paciente';
+import { Protocolo } from '../../../models/protocolo';
 
 @Component({
   selector: 'app-consultas-form',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './consultas-form.component.html',
   styleUrl: './consultas-form.component.scss'
 })
 export class ConsultasFormComponent {
+
+  modalRef!: MdbModalRef<any>; 
+
+  router = inject(Router);
+  rotaAtivada = inject(ActivatedRoute);
+
+  tituloComponente: string = "Nova Consulta";
+
+  consultaService = inject(ConsultaService);
+
+  @Input() consulta: Consulta = new Consulta();
+  @Output() retorno = new EventEmitter();
+
+  constructor(){
+
+  }
+
+  cadastrar() {
+    if (this.consulta) {
+      this.consultaService.save(this.consulta).subscribe({
+        next: mensagem => {
+          this.retorno.emit(mensagem);
+        },
+        error: erro => {
+          alert('Erro ao criar!');
+        }
+      });
+    } 
+  } 
+
+  atualizar(){
+    this.consultaService.update(this.consulta).subscribe({
+      next: mensagem =>{
+        
+        this.retorno.emit(mensagem);
+
+      },
+      error: erro =>{
+        alert('Erro ao atualizar');
+      }
+    });
+  }
 
 }
