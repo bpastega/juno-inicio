50ec82a8aa8baca84ecbd3da3bb7bf3f8ba658a8
bpastega <95308940+bpastega@users.noreply.github.com>
2025-05-17 21:36:32 -0300

    teste limitar login (black cat)

--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -4,13 +4,14 @@ import { Router } from '@angular/router';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms'; //Importado manualmente:: https://mdbootstrap.com/docs/angular/getting-started/modules-and-imports/
 import { LoginService } from '../../../auth/login.service';
 import { Login } from '../../../auth/login';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   login: Login = new Login();
@@ -18,24 +19,37 @@ export class LoginComponent {
   router = inject(Router); //semelhante ao Autowired no backend
   loginService = inject(LoginService);
 
-  constructor(){
+  constructor() {
     this.loginService.removerToken();
   }
 
-  logar(){
-    this.loginService.logar(this.login).subscribe({
-      next: token =>{
-        if(token){ //usuário e senha corretos
-          this.loginService.addToken(token);
-          this.router.navigate(['admin/dashboard'])
-        }
-        else{ //usuário ou senha incorretos
-          alert("Usuário ou senha incorretos");
-        }
-      },
-      error: erro =>{
-        alert("erro");
-      }
-    })
+  logar() {
+    if (this.loginService.hasPermission('black-cat-role')) {
+      Swal.fire({
+                        icon: "error",
+                        title: "Acesso restrito a detentores de cadastro Juno",
+                        text: "Acesso Negado!",
+                });
+    }
+    
+    else {
+      this.loginService.logar(this.login).subscribe({
+        next: (token) => {
+          if (token) {
+            //usuário e senha corretos
+            this.loginService.addToken(token);
+            this.router.navigate(['admin/dashboard']);
+          } else {
+            //usuário ou senha incorretos
+            alert('Usuário ou senha incorretos');
+          }
+        },
+        error: (erro) => {
+          alert('erro');
+        },
+      });
+    }
+
+    
   }
 }
